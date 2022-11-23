7a09794de2a6f529a9940dcb49a72f864883e67f
chuchma.l <chuchma.l@fce.vutbr.cz>
2022-11-23 15:22:10 +0100

    +fix Jura bold font

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Jura' },
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Jura:wght@400;700&display=swap' },
             { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
             { rel: 'icon" type="image/png', sizes: '32x32', href: '/favicon-32x32.png' },
             { rel: 'icon" type="image/png', sizes: '16x16', href: '/favicon-16x16.png' }
